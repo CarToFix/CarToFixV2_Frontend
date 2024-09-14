cdbaf549b84cd787e3e2dcb1ec9d9d0f3d02127d
lucassoriabusto <lucassoriabusto@hotmail.com>
2024-09-14 02:49:26 -0300

    Update

--- src/views/Settings.jsx
+++ src/views/Settings.jsx
@@ -5,9 +5,17 @@ import styled from 'styled-components';
 const Settings = () => {
     return(
         <Settings_Container>
-            <h3>Configuración del taller</h3>
-            <h3>Gestion de usuarios</h3>
-            <h3>Contactanos</h3>
+            <Box1>
+                <h3>X</h3>
+            </Box1>
+            <Box2>
+                <h3>Configuración del taller</h3>
+                <h3>Gestion de usuarios</h3>
+                <h3>Contactanos</h3>
+            </Box2>
+            <Box3>
+                <h2>Cerrar Sesión</h2>
+            </Box3>
         </Settings_Container>
     )
 };
@@ -15,15 +23,48 @@ const Settings = () => {
 export default Settings;
 
 const Settings_Container = styled.div`
-    justify-content: center; /* Centra horizontalmente */
-    align-items: center;     /* Centra verticalmente */
+    display: flex;
+    flex-direction: column; /* Alinea los elementos en una columna (verticalmente) */
+    justify-content: center; /* Centra verticalmente los elementos dentro del contenedor */
+    align-items: center;     /* Centra horizontalmente los elementos dentro del contenedor */
+
     height: 100%;
     width: 20vw;
+    min-width: 20vw;
     background-color: #ffffff;
-    border: 2px solid #2C518D;
+    border: 3px solid #2C518D;
     border-radius: 10px;
-    position: fixed;
-    z-index: 1000;
+    position: fixed; /* Posiciona el modal arriba del resto de elementos */
+    z-index: 1000; /* Mantiene el modal sobre cualquier elemento */ 
     left: 0;
+`
 
+const Box1 = styled.div`
+    height: 10vh;
+    width: 100%;
+    background-color: pink;
+`
+
+const Box2 = styled.div`
+    display: flex;
+    flex-direction: column; /* Alinea los elementos en columna */
+    justify-content: center; /* Centra verticalmente los elementos */
+    align-items: center; /* Centra horizontalmente los elementos */
+    height: 80vh; /* Ocupa el espacio restante debajo de la barra */
+    width: 100%; /* Ocupa todo el ancho disponible */
+   
+    background-color: #ffffff;
+    color: #2C518D;
+`
+
+const Box3 = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    height: 10vh;
+    width: 100%;
+    background: linear-gradient(70deg, #23406D, #335FA6);
+    color: white;
+    padding-bottom: 0px;
 `
